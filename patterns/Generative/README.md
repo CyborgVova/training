ce451802e5e3a02aaecb6dcd674e18ab6b4d39c6
CyborgVova <eroxin79@mail.ru>
2024-03-16 14:29:05 +0300

    change struct project, add split on view patterns(Generative)

---
+++ patterns/Generative/README.md
@@ -0,0 +1,7 @@
+### Порождающие паттерны
+
+1. Абстрактная фабрика(Abstract Factory)
+2. Строитель(Builder)
+3. Фабричный метод(Factory Method)
+4. Прототип(Prototype)
+5. Одиночка(Singleton)
--- patterns/Generative/singleton/singleton_test.go
+++ patterns/Generative/singleton/singleton_test.go
@@ -1,37 +1,37 @@
-package singleton
+package test
 
 import (
 	"testing"
+
+	"github.com/cyborgvova/training/patterns/Generative/singleton"
 )
 
 func TestSingleton(t *testing.T) {
 	tests := []struct {
-		tname     string
-		singleton *instance
-		id        string
-		name      string
+		tname string
+		id    string
+		name  string
 	}{
 		{
-			tname:     "singleton is nil",
-			singleton: GetInstance("2", "Seva"),
-			id:        "2",
-			name:      "Seva",
+			tname: "singleton is nil",
+			id:    "2",
+			name:  "Seva",
 		}, {
-			tname:     "have singleton instance",
-			singleton: GetInstance("1", "Jhon"),
-			id:        "2",
-			name:      "Seva",
+			tname: "have singleton instance",
+			id:    "1",
+			name:  "Jhon",
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.tname, func(t *testing.T) {
+			slt := singleton.GetInstance(test.id, test.name)
 			want := struct {
 				id   string
 				name string
 			}{
-				id:   test.id,
-				name: test.name,
+				id:   "2",
+				name: "Seva",
 			}
 			got := struct {
 				id   string
