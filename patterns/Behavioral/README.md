2ebd69f68b789130dd4e413623d638678640de57
CyborgVova <eroxin79@mail.ru>
2024-03-17 01:54:55 +0300

    start iterator pattern implementation

--- patterns/Behavioral/README.md
+++ patterns/Behavioral/README.md
@@ -2,3 +2,4 @@
 
 1. [Цепочка обязанносей(ChainOfResponsibility)](chainofresponsibility/README.md)
 2. [Команда(Command)](command/README.md)
+3. [Итератор(Iterator)](iterator/README.md)
---
+++ patterns/Behavioral/iterator/iterator_test.go
@@ -0,0 +1,12 @@
+package iterator
+
+import "testing"
+
+func TestIterator(t *testing.T) {
+	want := ""
+	got := ""
+
+	if want != got {
+		t.Errorf("want: %s, got: %s\n", want, got)
+	}
+}
