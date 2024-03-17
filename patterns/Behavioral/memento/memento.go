7a919310f163457b2a5d8b9a5f5d371a35b6a50d
CyborgVova <eroxin79@mail.ru>
2024-03-18 02:21:39 +0300

    add memento pattern

--- patterns/Behavioral/memento/memento.go
+++ patterns/Behavioral/memento/memento.go
@@ -1,3 +1,27 @@
 package memento
 
-type Memento interface{}
+type Object struct {
+	Field int
+}
+
+type states struct {
+	objects []Object
+}
+
+func NewStates(object Object) *states {
+	return &states{objects: []Object{object}}
+}
+
+func (s *states) Save(object Object) {
+	s.objects = append(s.objects, object)
+}
+
+func (s *states) GetLast() Object {
+	len := len(s.objects)
+	if len == 1 {
+		return s.objects[0]
+	}
+	object := s.objects[len-1]
+	s.objects = s.objects[:len-1]
+	return object
+}
--- patterns/Behavioral/memento/memento_test.go
+++ patterns/Behavioral/memento/memento_test.go
@@ -1,12 +1,46 @@
 package memento
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMemento(t *testing.T) {
-	want := ""
-	got := ""
+	object := Object{Field: 100}
+	state := NewStates(object)
+	object.Field = 200
+	state.Save(object)
+	object.Field = 300
+	state.Save(object)
 
-	if want != got {
-		t.Errorf("want: %s, got: %s\n", want, got)
+	tests := []struct {
+		name  string
+		field int
+		state Object
+	}{
+		{
+			name:  "First",
+			field: 300,
+			state: state.GetLast(),
+		}, {
+			name:  "Second",
+			field: 200,
+			state: state.GetLast(),
+		}, {
+			name:  "Third",
+			field: 100,
+			state: state.GetLast(),
+		},
 	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			want := test.field
+			got := test.state.Field
+
+			if want != got {
+				t.Errorf("want: %d, got: %d\n", want, got)
+			}
+		})
+	}
+
 }
