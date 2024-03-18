155d76212673320c0197d28462b90cff1acbce18
CyborgVova <eroxin79@mail.ru>
2024-03-19 01:54:17 +0300

    add pattern state and readme file fot him

--- patterns/Behavioral/state/state.go
+++ patterns/Behavioral/state/state.go
@@ -1,7 +1,42 @@
 package state
 
-type State interface{}
+type Object struct {
+	State State
+}
 
-type ClassA struct{}
+func NewObject() *Object {
+	return &Object{State: &StateA{}}
+}
 
-type ClassB struct{}
+func (o *Object) Trigger() {
+	o.State = o.State.Trigger()
+}
+
+func (o *Object) GetState() string {
+	return o.State.GetState()
+}
+
+type State interface {
+	Trigger() State
+	GetState() string
+}
+
+type StateA struct{}
+
+func (s *StateA) GetState() string {
+	return "RED"
+}
+
+func (s *StateA) Trigger() State {
+	return &StateB{}
+}
+
+type StateB struct{}
+
+func (s *StateB) GetState() string {
+	return "GREEN"
+}
+
+func (s *StateB) Trigger() State {
+	return &StateA{}
+}
--- patterns/Behavioral/state/state_test.go
+++ patterns/Behavioral/state/state_test.go
@@ -3,10 +3,35 @@ package state
 import "testing"
 
 func TestState(t *testing.T) {
-	want := ""
-	got := ""
+	object := NewObject()
 
-	if want != got {
-		t.Errorf("want: %s, got: %s\n", want, got)
+	tests := []struct {
+		name string
+		want string
+		got  string
+	}{
+		{
+			name: "First",
+			want: "GREEN",
+		}, {
+			name: "Second",
+			want: "RED",
+		}, {
+			name: "Third",
+			want: "GREEN",
+		}, {
+			name: "Fourth",
+			want: "RED",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			object.Trigger()
+			got := object.GetState()
+			if test.want != got {
+				t.Errorf("want: %s, got: %s\n", test.want, got)
+			}
+		})
 	}
 }
