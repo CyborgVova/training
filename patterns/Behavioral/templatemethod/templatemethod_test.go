bc05b8ad2acd32ad2dd733e460948118b8f95070
CyborgVova <eroxin79@mail.ru>
2024-03-19 17:11:52 +0300

    add template for templatemethod pattern

---
+++ patterns/Behavioral/templatemethod/templatemethod_test.go
@@ -0,0 +1,12 @@
+package templatemethod
+
+import "testing"
+
+func TestTemplateMethod(t *testing.T) {
+	want := ""
+	got := ""
+
+	if want != got {
+		t.Errorf("want: %s, got: %s\n", want, got)
+	}
+}
