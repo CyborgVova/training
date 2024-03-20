0a1b2c10d5d585c00807930315a6597be61ec741
Delilah Lollys <delilahl@at-e3.msk.21-school.ru>
2024-03-20 16:52:57 +0300

    add adapter pattern, test and readme files

--- patterns/Structural/adapter/adapter.go
+++ patterns/Structural/adapter/adapter.go
@@ -1,3 +1,26 @@
 package adapter
 
-type Adapter struct{}
+type Usb interface {
+	ConnectToComputer() string
+}
+
+type CardReader struct {
+	SdCard *SdCard
+}
+
+func (c *CardReader) ConnectToComputer() (result string) {
+	result += "Connection ...\n"
+	result += c.SdCard.ConnectToDevice()
+	result += c.SdCard.CopyData()
+	return
+}
+
+type SdCard struct{}
+
+func (f *SdCard) ConnectToDevice() string {
+	return "Connection is done\n"
+}
+
+func (f *SdCard) CopyData() string {
+	return "Data was copied\n"
+}
--- patterns/Structural/adapter/adapter_test.go
+++ patterns/Structural/adapter/adapter_test.go
@@ -3,8 +3,13 @@ package adapter
 import "testing"
 
 func TestAdapter(t *testing.T) {
-	want := ""
-	got := ""
+	sdCard := &SdCard{}
+	cardReader := &CardReader{SdCard: sdCard}
+
+	want := "Connection ...\n" +
+		"Connection is done\n" +
+		"Data was copied\n"
+	got := cardReader.ConnectToComputer()
 
 	if want != got {
 		t.Errorf("want: %s, got: %s\n", want, got)
