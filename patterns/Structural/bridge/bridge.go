3b4678085eca62572dc843dd48d367b21791411b
Delilah Lollys <delilahl@at-e3.msk.21-school.ru>
2024-03-20 18:23:15 +0300

    add bridge pattern and description

--- patterns/Structural/bridge/bridge.go
+++ patterns/Structural/bridge/bridge.go
@@ -1,2 +1,37 @@
 package bridge
 
+type Engine interface {
+	EngineType() string
+}
+
+type PetrolEngine struct{}
+
+func (p *PetrolEngine) EngineType() string {
+	return "Petrol engine\n"
+}
+
+type DieselEngine struct{}
+
+func (d *DieselEngine) EngineType() string {
+	return "Diesel engine\n"
+}
+
+type Auto interface {
+	Complectation() string
+}
+
+type BMW struct {
+	Engine Engine
+}
+
+func (b *BMW) Complectation() string {
+	return "It is BMW with " + b.Engine.EngineType()
+}
+
+type Audi struct {
+	Engine Engine
+}
+
+func (a *Audi) Complectation() string {
+	return "It is Audi with " + a.Engine.EngineType()
+}
--- patterns/Structural/bridge/bridge_test.go
+++ patterns/Structural/bridge/bridge_test.go
@@ -3,10 +3,38 @@ package bridge
 import "testing"
 
 func TestBridge(t *testing.T) {
-	want := ""
-	got := ""
+	tests := []struct {
+		name string
+		want string
+		car  Auto
+	}{
+		{
+			name: "Audi Petrol",
+			want: "It is Audi with Petrol engine\n",
+			car:  &Audi{Engine: &PetrolEngine{}},
+		}, {
+			name: "Audi Petrol",
+			want: "It is Audi with Diesel engine\n",
+			car:  &Audi{Engine: &DieselEngine{}},
+		}, {
+			name: "BMW Petrol",
+			want: "It is BMW with Petrol engine\n",
+			car:  &BMW{Engine: &PetrolEngine{}},
+		}, {
+			name: "BMW Petrol",
+			want: "It is BMW with Diesel engine\n",
+			car:  &BMW{Engine: &DieselEngine{}},
+		},
+	}
 
-if want != got {
-	t.Errorf("want: %s, got: %s\n", want, got)
-}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			got := test.car.Complectation()
+
+			if test.want != got {
+				t.Errorf("want: %s, got: %s\n", test.want, got)
+			}
+		})
+	}
 }
