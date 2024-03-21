d15dec47b8bce19e1e00489f4490b069303fccb7
CyborgVova <eroxin79@mail.ru>
2024-03-22 01:50:01 +0300

    add facade pattern and readme file

--- patterns/Structural/README.md
+++ patterns/Structural/README.md
@@ -4,3 +4,4 @@
 2. [Мост (Bridge)](bridge/README.md)
 3. [Компоновщик (Composite)](composite/README.md)
 4. [Декоратор (Decorator)](decorator/README.md)
+5. [Фасад (Facade)](facade/README.md)
--- patterns/Structural/facade/facade_test.go
+++ patterns/Structural/facade/facade_test.go
@@ -3,10 +3,27 @@ package facade
 import "testing"
 
 func TestFacade(t *testing.T) {
-	want := ""
-	got := ""
+	facade := NewFacade("Igor Pupkin")
 
-	if want != got {
-		t.Errorf("want: %s, got: %s\n", want, got)
+	myDay := facade.Home.Out() +
+		facade.Auto.In() +
+		facade.Auto.Out() +
+		facade.Job.In() +
+		facade.Job.Out() +
+		facade.Auto.In() +
+		facade.Auto.Out() +
+		facade.Home.In()
+
+	want := "Igor Pupkin left home\n" +
+		"Igor Pupkin got into the car\n" +
+		"Igor Pupkin got out of the car\n" +
+		"Igor Pupkin came job\n" +
+		"Igor Pupkin left job\n" +
+		"Igor Pupkin got into the car\n" +
+		"Igor Pupkin got out of the car\n" +
+		"Igor Pupkin came home\n"
+
+	if want != myDay {
+		t.Errorf("want: %s, got: %s\n", want, myDay)
 	}
 }
