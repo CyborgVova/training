53bdb638deb03561eac9247f84ee422b5f7d705a
CyborgVova <eroxin79@mail.ru>
2024-03-21 23:12:44 +0300

    add decorator pattern and readme file

--- patterns/Structural/decorator/decorator.go
+++ patterns/Structural/decorator/decorator.go
@@ -1,3 +1,20 @@
 package decorator
 
-type Decorator interface{}
+type Class interface {
+	Print(string) string
+}
+
+type Object struct{}
+
+func (o *Object) Print(text string) string {
+	return text
+}
+
+type Decorator struct {
+	Apostrof string
+	Object   *Object
+}
+
+func (d *Decorator) Print(text string) string {
+	return d.Apostrof + d.Object.Print(text) + d.Apostrof
+}
--- patterns/Structural/decorator/decorator_test.go
+++ patterns/Structural/decorator/decorator_test.go
@@ -3,10 +3,49 @@ package decorator
 import "testing"
 
 func TestDecorator(t *testing.T) {
-	want := ""
-	got := ""
 
-	if want != got {
-		t.Errorf("want: %s, got: %s\n", want, got)
+	tests := []struct {
+		name     string
+		text     string
+		apostrof string
+		want     string
+	}{
+		{
+			name:     "one",
+			text:     "First row",
+			apostrof: "\"",
+			want:     "\"First row\"",
+		}, {
+			name:     "two",
+			text:     "Second row",
+			apostrof: "`",
+			want:     "`Second row`",
+		}, {
+			name:     "three",
+			text:     "Third row",
+			apostrof: "'",
+			want:     "'Third row'",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			object := &Object{}
+			decorator := &Decorator{Apostrof: test.apostrof, Object: object}
+
+			want := test.text
+			got := object.Print(test.text)
+
+			if want != got {
+				t.Errorf("want: %s, got: %s\n", want, got)
+			}
+
+			want = test.want
+			got = decorator.Print(test.text)
+
+			if want != got {
+				t.Errorf("want: %s, got: %s\n", want, got)
+			}
+		})
 	}
 }
