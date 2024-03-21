c87e5fa3e6bde7730e33698db8d1af4c4de5bf39
CyborgVova <eroxin79@mail.ru>
2024-03-21 15:12:58 +0300

    add composite pattern

--- patterns/Structural/composite/composite_test.go
+++ patterns/Structural/composite/composite_test.go
@@ -1,11 +1,47 @@
 package composite
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestComposite(t *testing.T) {
-	want := ""
-	got := ""
+	chH := &Character{Character: 'H'}
+	che := &Character{Character: 'e'}
+	chl := &Character{Character: 'l'}
+	cho := &Character{Character: 'o'}
+	chSpace := &Character{Character: ' '}
+	chComma := &Character{Character: ','}
+	chW := &Character{Character: 'W'}
+	chr := &Character{Character: 'r'}
+	chd := &Character{Character: 'd'}
+	chExcl := &Character{Character: '!'}
+	chV := &Character{Character: 'V'}
+	chv := &Character{Character: 'v'}
+	cha := &Character{Character: 'a'}
 
+	hello := &Word{}
+	hello.Add(chH, che, chl, chl, cho)
+
+	world := &Word{}
+	world.Add(chW, cho, chr, chl, chd)
+
+	vova := &Word{}
+	vova.Add(chV, cho, chv, cha)
+
+	sentence := &Word{}
+	sentence.Add(hello, chComma, chSpace, world, chSpace, chExcl, chExcl, chExcl)
+
+	want := "Hello, World !!!"
+	got := sentence.Print()
+	if want != got {
+		t.Errorf("want: %s, got: %s\n", want, got)
+	}
+
+	sentence.Remove(chSpace, world, chSpace, chExcl, chExcl, chExcl)
+	sentence.Add(chSpace, vova, chSpace, chExcl, chExcl, chExcl)
+
+	want = "Hello, Vova !!!"
+	got = sentence.Print()
 	if want != got {
 		t.Errorf("want: %s, got: %s\n", want, got)
 	}
