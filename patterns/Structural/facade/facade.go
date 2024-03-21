4ea678f2b775a5f51bfbc66f708e3fa3251f2994
CyborgVova <eroxin79@mail.ru>
2024-03-21 23:32:27 +0300

    add template for facade pattern

---
+++ patterns/Structural/facade/facade.go
@@ -0,0 +1,3 @@
+package facade
+
+type Facade struct{}
---
+++ patterns/Structural/facade/facade_test.go
@@ -0,0 +1,12 @@
+package facade
+
+import "testing"
+
+func TestFacade(t *testing.T) {
+	want := ""
+	got := ""
+
+	if want != got {
+		t.Errorf("want: %s, got: %s\n", want, got)
+	}
+}
