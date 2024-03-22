d7b23a1f8942aa2902724f559e3bc4a6f23d4cd2
CyborgVova <eroxin79@mail.ru>
2024-03-22 14:46:14 +0300

    add flyweight pattern

--- patterns/Structural/README.md
+++ patterns/Structural/README.md
@@ -5,4 +5,4 @@
 3. [Компоновщик (Composite)](composite/README.md)
 4. [Декоратор (Decorator)](decorator/README.md)
 5. [Фасад (Facade)](facade/README.md)
-6. [Приспособленец (FlyWeight)](flyweight/README.md)
+6. [Легковес (FlyWeight)](flyweight/README.md)
--- patterns/Structural/flyweight/flyweight_test.go
+++ patterns/Structural/flyweight/flyweight_test.go
@@ -1,12 +1,48 @@
 package flyweight
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestFlyWeight(t *testing.T) {
-	want := ""
-	got := ""
+	circle := &Circle{Radius: 10.0}
+	rectangle := &Rectangle{SideA: 10.0, SideB: 10.0}
+	treeangle := &Treeangle{Side: 10.0, Height: 10.0}
 
-	if want != got {
-		t.Errorf("want: %s, got: %s\n", want, got)
+	flyWeight := NewFlyWeight()
+	flyWeight.Add("circle", circle)
+	flyWeight.Add("rectangle", rectangle)
+	flyWeight.Add("treeangle", treeangle)
+
+	tests := []struct {
+		name string
+		want float64
+	}{
+		{
+			name: "circle",
+			want: 314.0,
+		}, {
+			name: "rectangle",
+			want: 100.0,
+		}, {
+			name: "treeangle",
+			want: 50.0,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			element, err := flyWeight.GetElement(test.name)
+			if err != nil {
+				t.Error("unexpected error: ", err.Error())
+			}
+
+			got := element.Area()
+
+			if test.want != math.Round(got) {
+				t.Errorf("want: %.6f, got: %.6f\n", test.want, got)
+			}
+		})
 	}
 }
