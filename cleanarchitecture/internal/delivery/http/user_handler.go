49e4158869a8494da777d20579ab4d023ef85f59
Delilah Lollys <delilahl@oa-e6.msk.24-school.ru>
2024-05-27 16:12:58 +0300

    add implementations all structures in cleararchitecture

--- cleanarchitecture/internal/delivery/http/user_handler.go
+++ cleanarchitecture/internal/delivery/http/user_handler.go
@@ -1,1 +1,13 @@
 package http
+
+import (
+	"github.com/cyborgvova/training/cleanarchitecture/internal/usecase"
+)
+
+type UserHandler struct{}
+
+func NewUserHandler(userUC *usecase.UserUseCase) UserHandler {
+	return UserHandler{}
+}
+
+func (uh *UserHandler) Run(port string)
--- cleanarchitecture/internal/usecase/user_usecase.go
+++ cleanarchitecture/internal/usecase/user_usecase.go
@@ -1,1 +1,11 @@
 package usecase
+
+import (
+	"github.com/cyborgvova/training/cleanarchitecture/pkg/database"
+)
+
+type UserUseCase struct{}
+
+func NewUserUseCase(userRepo *database.UserRepository) *UserUseCase {
+	return &UserUseCase{}
+}
